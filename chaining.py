59425c91998199de05f19bbf5b6129f73d246dd4
Omar.M.Bi <63540588+OmarBishawi@users.noreply.github.com>
2024-06-04 17:52:29 +0300

    chaining assignment-OOP

---
+++ chaining.py
@@ -0,0 +1,32 @@
+class user:
+    def __init__(self,name,balance):
+        self.name=name
+        self.balance=balance
+
+    def make_withdrawal(self,amount):
+        if amount>self.balance:
+            print("insufficent funds .withdrawal canceled")
+        else:
+            self.balance -= amount
+            return self
+
+    def display_user_balance(self):
+        print(f"user: {self.name}, balance :${self.balance}") 
+        return self       
+
+    def transfer_money(self,other_user,amount):
+        if amount>self.balance:
+            print("insufficent funds. withdrawal canceled")
+        else:
+            self.balance -= amount
+            other_user.balance +=amount
+            return self
+
+user1 = user("omar",1000)
+user2 = user("bob",500)
+
+
+user1.make_withdrawal(200).display_user_balance().transfer_money(user2,300).display_user_balance()
+user2.make_withdrawal(100).display_user_balance().display_user_balance()
+
+
